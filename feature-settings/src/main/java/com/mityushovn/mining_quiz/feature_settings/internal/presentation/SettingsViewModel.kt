8f09fe854815a7548cd4b20bdafaffbc67802329
NikitaMityushov <nikitamityushov@gmail.com>
2022-11-30 13:57:44 +0300

    settings_screen #3 navigation to the Settings screen is added

---
+++ feature-settings/src/main/java/com/mityushovn/mining_quiz/feature_settings/internal/presentation/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.mityushovn.mining_quiz.feature_settings.internal.presentation
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
